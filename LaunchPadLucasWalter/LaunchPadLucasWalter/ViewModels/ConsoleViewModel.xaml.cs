a19e707300f63486e653b1c4926e8f4dbda96f9c
Lucas <lucaswyland11@gmail.com>
2014-09-20 05:09:22 -0500

    Initialized ConsoleModel in its ViewModel

--- LaunchPadLucasWalter/LaunchPadLucasWalter/ViewModels/ConsoleViewModel.xaml.cs
+++ LaunchPadLucasWalter/LaunchPadLucasWalter/ViewModels/ConsoleViewModel.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+using LaunchPadLucasWalter.Models;
+
 namespace LaunchPadLucasWalter.ViewModels
 {
     /// <summary>
@@ -19,10 +21,13 @@ namespace LaunchPadLucasWalter.ViewModels
     /// </summary>
     public partial class ConsoleViewModel : Window
     {
+        private ConsoleModel Model;
+
         public ConsoleViewModel()
         {
             InitializeComponent();
             this.DataContext = new ConsoleViewModel();
+            Model = new ConsoleModel();
         }
     }
 }
