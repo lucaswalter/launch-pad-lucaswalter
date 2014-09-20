d83bfba6efaca06a47dd8616604dcad0456287e3
Lucas <lucaswyland11@gmail.com>
2014-09-20 06:40:00 -0500

    Fixed ViewModels.

--- LaunchPadLucasWalter/LaunchPadLucasWalter/ViewModels/ConsoleViewModel.cs
+++ LaunchPadLucasWalter/LaunchPadLucasWalter/ViewModels/ConsoleViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LaunchPadLucasWalter.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace LaunchPadLucasWalter.ViewModels
 {
-    class ConsoleViewModel
+    public class ConsoleViewModel
     {
+        private ConsoleModel Model;
+
+        public ConsoleViewModel()
+        {
+            Model = new ConsoleModel();
+        }
     }
 }
