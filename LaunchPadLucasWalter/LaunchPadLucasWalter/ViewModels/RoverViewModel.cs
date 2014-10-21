8fa7c49d9cfde634bf43eb9995e6df580f18a51b
Lucas <lucaswyland11@gmail.com>
2014-10-21 14:20:48 -0500

    Added RoverViewModel Properties
    Added the corresponding properties into RoverViewModel from RoverModel.

--- LaunchPadLucasWalter/LaunchPadLucasWalter/ViewModels/RoverViewModel.cs
+++ LaunchPadLucasWalter/LaunchPadLucasWalter/ViewModels/RoverViewModel.cs
@@ -7,6 +7,66 @@ namespace LaunchPadLucasWalter.ViewModels
     {
         private RoverModel Model;
 
+        public string RoverName
+        {
+            get
+            {
+                return Model.roverName;
+            }
+
+            set
+            {
+                Model.roverName = value;
+                NotifyOfPropertyChange();
+
+            }
+        }
+
+        public int Speed
+        {
+            get
+            {
+                return Model.speed;
+            }
+
+            set
+            {
+                Model.speed = value;
+                NotifyOfPropertyChange();
+
+            }
+        }
+
+        public double Temperature
+        {
+            get
+            {
+                return Model.temperature;
+            }
+
+            set
+            {
+                Model.temperature = value;
+                NotifyOfPropertyChange();
+
+            }
+        }
+
+        public bool IsConnected
+        {
+            get
+            {
+                return Model.isConnected;
+            }
+
+            set
+            {
+                Model.isConnected = value;
+                NotifyOfPropertyChange();
+
+            }
+        }
+
         public RoverViewModel()
         {
             Model = new RoverModel();
