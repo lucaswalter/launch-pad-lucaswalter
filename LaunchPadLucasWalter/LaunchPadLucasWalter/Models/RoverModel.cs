1c90486a10f0899d963508b09af6a3563068b501
Lucas <lucaswyland11@gmail.com>
2014-10-21 14:07:13 -0500

    Cleaned Up Usings In RoverModel

--- LaunchPadLucasWalter/LaunchPadLucasWalter/Models/RoverModel.cs
+++ LaunchPadLucasWalter/LaunchPadLucasWalter/Models/RoverModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LaunchPadLucasWalter.Models
+﻿namespace LaunchPadLucasWalter.Models
 {
     class RoverModel
     {
