87ab3696bef8f29ff589bb16d370b47c33808028
Lucas <lucaswyland11@gmail.com>
2014-10-21 14:16:00 -0500

    Declared Internal Fields for RoverModel

--- LaunchPadLucasWalter/LaunchPadLucasWalter/Models/RoverModel.cs
+++ LaunchPadLucasWalter/LaunchPadLucasWalter/Models/RoverModel.cs
@@ -2,5 +2,9 @@
 {
     class RoverModel
     {
+        internal string roverName = string.Empty;
+        internal int speed = 0;
+        internal double temperature = 0;
+        internal bool isConnected = false;
     }
 }
